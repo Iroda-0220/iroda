78e455ea0af1f6261fb2395f177328ad9361d8d5
Iroda-0220 <161613079+Iroda-0220@users.noreply.github.com>
2024-02-28 20:15:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,133 @@
+// javascript dasturlash tili -> c++ python,php ,c# java;
+//javascript != java;
+// 1995 yil Brenden Eich;
+//js browserda ishlaydigan dasturlash tili (yagona)
+// frontend backend (node js)
+//esma script 
+// es5,es6 ... es11
+//kompliator
+//interpretator (browser)
+// ozgaruvchilar -> variables
+
+
+//declaring
+// var < 2015
+// let > 2015
+// const (ozgarmas elon qilish un)
+
+
+
+// DATA TYPES  -> axborot turlari
+
+//2 ga bolinadi
+    //primitive
+    // not primitive
+
+
+//number (int,intefer) - 6.8 82384,323,
+// string-"IT LIVE ",'it live '
+// boolen - true.false,=> rost yolgon, true==1 false==0=> mantiqiy axborot turi
+//null - bosh
+//bigINT 
+
+// undefined
+//symbol
+
+
+
+//object - {}
+// array - axborot turi object ga kiradi
+
+
+
+// var num=20; //sonli
+//    num=30;
+//    console.log(num) // browserni console ga chiqaradi
+// var name="IT LIVE";
+//    console.log(name)
+// var dev="web"; //matn axborot turi
+//    console.log(dev)
+
+
+
+//    const pi=3.14; // sonli axborot turi
+//     console.log(pi)
+
+//     console.warn('error')
+//     console.error('xatolik')
+
+
+//     document.write(pi)
+//     alert('hello')
+
+
+// var a=10;
+// var b=20;
+// var plus =a+b;
+//   console.log(plus)
+// var minus =a+b;
+//   console.log(minus)
+
+// var kop=a*blur;
+
+
+// condition -> shart va shartli operatorlari 
+//  shart operatorlari "==" qatiy axborot turuga bogliq emas
+//  m:5==5 true , 5==="5" falce 10===20 false
+// inkor opersstorlari != true ni false ga, false ni true ga aylantiradi
+
+// if
+// if (condition){
+    // ish
+// }
+// if (4==5){
+    // console.log("true")
+//}else{
+    // console.log('false')
+// }
+
+// let user = promt ('salom'); // ekranga inputli modal chiqaradi 
+
+
+// let a = +promt('birinchi sonni kiriting');// +"asd"
+// let b = +promt("ikkinchi sonni kiriting");
+
+
+//let res;
+// if(a>b){
+//     res =a+blur;
+// }else{
+//     res=b-a;
+// }
+
+// console.log(res)
+// alert(res)
+// isNaN()son emasligini tekshiradi
+
+
+// let user =promt("salomlashing")
+// if(user == "salom"){
+//     console.log("xush kelibsiz")
+// }else if(user == "hello"){
+//     console.log("welcome")
+// }else if(user  == "privet"){
+//     console.log("xush kelibsiz ru")
+// }else{
+//     console.log('salomlashishni organing')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
